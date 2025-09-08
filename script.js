0045b9db65d16e0e40de10178604b752f3a8686c
abhayminer01 <abhayvapvl@gmail.com>
2025-09-08 09:22:50 +0530

    scripting completed

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function computerChoice() {
     return choice;
 }
 
+var win = 0, lose = 0, tie = 0;
+const winComponent = document.querySelector('.win');
+const loseComponent = document.querySelector('.lose');
+const tieComponent = document.querySelector('.tie');
+
 window.addEventListener('click', function(e) {
     const result = document.querySelector('.result');
     const userChoice = e.target.innerHTML;
@@ -18,4 +23,34 @@ window.addEventListener('click', function(e) {
 
     const res = `You : ${userChoice} - ${computer} : Computer`;
     result.innerHTML = res;
+
+    findResult(userChoice, computer);
+    updateResult();
 });
+
+
+function findResult(user, computer) {
+    if(user === computer) {
+        tie += 1;
+    } else if(user === 'Rock' && computer === 'Paper') {
+        lose += 1;
+    } else if(user === 'Paper' && computer === 'Rock') {
+        win += 1;
+    } else if(user === 'Scissor' && computer === 'Paper') {
+        win += 1;
+    } else if(user === 'Paper' && computer === 'Scissor') {
+        lose += 1;
+    } else if(user === 'Scissor' && computer === 'Rock') {
+        lose += 1;
+    } else if(user === 'Rock' && computer === 'Scissor') {
+        win += 1;
+    } else {
+        return;
+    }
+}
+
+function updateResult() {
+    winComponent.innerHTML = "Win : " + win;
+    loseComponent.innerHTML = "Lose : " + lose;
+    tieComponent.innerHTML = "Tie : " + tie;
+}
