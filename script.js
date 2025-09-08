c13ef133ac3b5b1734108a1e69b1c34f005d9460
abhayminer01 <abhayvapvl@gmail.com>
2025-09-08 07:42:54 +0530

    random computer choice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function computerChoice() {
     let choice;
-    const random = Math.random();
-    console.log(random);
+    const random = Math.floor(Math.random() * 3);
+    if(random === 0) {
+        choice = 'rock';
+    } else if(random === 1) {
+        choice = 'paper';
+    } else {
+        choice = 'scissor';
+    };
     return choice;
 }
 
-
-computerChoice();
