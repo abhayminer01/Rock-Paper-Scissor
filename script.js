5fb0578941dea159b75cbeb6b6607250196b823f
abhayminer01 <abhayvapvl@gmail.com>
2025-09-08 07:52:51 +0530

    idk

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ function computerChoice() {
     let choice;
     const random = Math.floor(Math.random() * 3);
     if(random === 0) {
-        choice = 'rock';
+        choice = 'Rock';
     } else if(random === 1) {
-        choice = 'paper';
+        choice = 'Paper';
     } else {
-        choice = 'scissor';
+        choice = 'Scissor';
     };
     return choice;
 }
 
+window.addEventListener('click', function(e) {
+    const result = document.querySelector('.result');
+    const userChoice = e.target.innerHTML;
+    const computer = computerChoice();
+
+    const res = `You : ${userChoice} - ${computer} : Computer`;
+    result.innerHTML = res;
+});
